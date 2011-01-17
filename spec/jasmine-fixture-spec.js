79acb7a75f2a1bc6706f657c5c019232d35f3036
Justin Searls <searls@gmail.com>
2011-01-17 12:56:11 -0500

    added a spec around this change

--- spec/jasmine-fixture-spec.js
+++ spec/jasmine-fixture-spec.js
@@ -1,5 +1,16 @@
 describe('Jasmine Fixture',function(){
   it('is named $.jasmine',function(){
     expect($.jasmine).toBeDefined();
+  });
+  
+  describe('#inject',function(){
+    var result;
+    beforeEach(function(){
+      result = $.jasmine.inject('<div class="pants"></div>');
+    });
+    it('returns the injected HTML as a jQuery result',function(){      
+      expect(result.hasClass('pants')).toBe(true);
+    });
   })
+  
 });
