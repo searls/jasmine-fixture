7a1a6c63f3dc34a42041bd7f4af9fcfb3b25cf0e
Justin Searls <searls@gmail.com>
2011-10-09 10:02:55 -0400

    decrufting namespace a bit… tried pulling in jquery-jasmine, but that absolutely cannot support two queries at once.

--- spec/jasmine-fixture-spec.js
+++ spec/jasmine-fixture-spec.js
@@ -8,16 +8,54 @@ describe('Jasmine Fixture',function(){
     });
 
     describe('#inject',function(){
-      var result;
-      beforeEach(function(){
-        result = $.jasmine.inject('<div class="pants"></div>');
+      var $result;
+
+      context('provided an HTML string', function(){
+        beforeEach(function(){
+         $result = $.jasmine.inject('<div class="pants"></div>');
+        });
+
+        it('returns the injected HTML as a jQuery result',function(){
+          expect($result.is('div.pants')).toBe(true);
+        });
       });
-      it('returns the injected HTML as a jQuery result',function(){
-        expect(result.hasClass('pants')).toBe(true);
+
+      context('provided a string argument', function(){
+        beforeEach(function(){
+          $result = $.jasmine.inject('pants');
+        });
+
+        it('is a div',function(){
+          expect($result.is('div')).toBe(true);
+        });
+
+        it('uses the passed string as its id', function(){
+          expect($result.hasClass('pants')).toBe(true);
+        });
+
+        it('is contained by the body', function(){
+          expect($.contains(document.body,$result[0])).toBe(true);
+        });
       });
+
+
     })
 
-    it("tidies up (the page no longer contains the injected content on the page)", function() {
+    describe("$.fn.inject",function(){
+      var $container,$result;
+      beforeEach(function(){
+        $container = $.jasmine.inject('phone');
+
+        $result = $container.inject('fax');
+      });
+
+      it('appends the injected as a child of the container',function(){
+        expect($container.find('.fax').length).toBe(1);
+      });
+    });
+
+
+    it("afterward, it tidies up (the page no longer contains the injected content on the page)", function() {
       expect($('.pants').length).toBe(0);
     });
 
@@ -26,7 +64,8 @@ describe('Jasmine Fixture',function(){
       beforeEach(function() {
         cachedCopy = window.jasmineFixture;
 
-        subject = window.jasmineFixture($)
+        subject = window.jasmineFixture($);
+
         noConflicted = subject.noConflict();
       });
 
