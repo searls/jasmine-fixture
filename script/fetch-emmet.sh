55f26b8d5428ec98fa538a264692de9ff011b3bf
Justin Searls <searls@gmail.com>
2016-01-15 14:58:38 -0500

    undo the tag stuff, since emmet 1.3.0 doesn't work

--- script/fetch-emmet.sh
+++ script/fetch-emmet.sh
@@ -6,7 +6,8 @@ mkdir -p tmp
 echo "<---- cloning emmet to tmp/emmet"
 git clone git@github.com:emmetio/emmet.git tmp/emmet || true
 cd tmp/emmet
-latestRelease="$(git describe --tags `git rev-list --tags --max-count=1`)"
+#latestRelease="$(git describe --tags `git rev-list --tags --max-count=1`)"
+latestRelease="master" #<-- turns out, the latest release is busted. :-(
 echo "<---- checkout latest tag: '$latestRelease'"
 git checkout "$latestRelease"
 echo "<---- installing dependencies"
