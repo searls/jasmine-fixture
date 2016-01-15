9a3a69143bf1c0617d8ef901dba20f267e13a041
Justin Searls <searls@gmail.com>
2016-01-15 14:14:03 -0500

    improve the emmet script

--- script/fetch-emmet.sh
+++ script/fetch-emmet.sh
@@ -1,10 +1,19 @@
-#!/usr/bin/env bash -e
+#!/usr/bin/env bash
+
+set -e
 
 mkdir tmp
+echo "<---- cloning emmet to tmp/emmet"
 git clone git@github.com:emmetio/emmet.git tmp/emmet
 cd tmp/emmet
+latestRelease="$(git describe --tags `git rev-list --tags --max-count=1`)"
+echo "<---- checkout latest tag: '$latestRelease'"
+git checkout "$latestRelease"
+echo "<---- installing dependencies"
 npm i
+echo "<---- building emmet"
 $(npm bin)/gulp app
 cp dist/emmet-app.js ../../app/js/emmet.js
 cd ../..
+git commit -m "emmet $latestRelease" -- app/js/emmet.js || true
 rm -rf tmp
